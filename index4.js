9bce67cd8a982073f7090472564103b730ba98dd
NazarProton <nproton2012@gmail.com>
2021-06-08 00:17:19 +0300

    NP

--- index4.js
+++ index4.js
@@ -4,26 +4,26 @@
 
 // console.log(b);
 
-function z(n) {
-  if (n >= 0) return n;
-  else return 0;
-}
+// function z(n) {
+//   if (n >= 0) return n;
+//   else return 0;
+// }
 
-console.log(z(-3));
-console.log(z(4));
+// console.log(z(-3));
+// console.log(z(4));
 
-const z2 = (n) => (n >= 0 ? n : 0)
+// const z2 = (n) => (n >= 0 ? n : 0)
 
-console.log(z2(-3));
-console.log(z2(4));
+// console.log(z2(-3));
+// console.log(z2(4));
 
-const b = [2, 4, 5, 6, 7, 8];
-let c = b.map(item => ({'num': item}))
-console.log(c);
+// const b = [2, 4, 5, 6, 7, 8];
+// let c = b.map(item => ({'num': item}))
+// console.log(c);
 
 document.querySelector('#test').addEventListener
 ('click' , () => {
-    fetch('file.json')
+    fetch('./file.json')
         .then(res => res.json())
         .then(file => {
             file.forEach(item => {
